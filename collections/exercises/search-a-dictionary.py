5d4cfe605667e9a2550df24d53bfe793f607e8ca
Sudha <pereddymadhu@gmail.com>
2025-11-06 15:31:25 -0500

    Python Assignment

--- collections/exercises/search-a-dictionary.py
+++ collections/exercises/search-a-dictionary.py
@@ -11,22 +11,42 @@ flavors = {
 
 ## Set a variable called choice to the flavor you want to search for.
 
+choice = 'saffron'  
 ## Use an if statement to check if choice is in the flavors dictionary.
+if choice in flavors:
+    print(choice, flavors[choice])
 
 ## If it is, set another variable called cost to the value associated with choice.
-
 ## If it isn’t, set cost to 0.
-
+if choice in flavors:
+    cost = flavors[choice]   
+else:
+    cost = 0  
 ## Print the cost.
-
+print("Cost:", cost)
 ### Search a Dictionary Part 2:
 
 ## Initialize two variables: highest_cost to 0 and fanciest to an empty string.
+highest_cost = 0
+fanciest = {}
 
 ## Loop through the flavors dictionary using a for loop.
+for key in flavors.keys():
+    print(key, flavors[key])
 
 ## For each flavor, check if its price is higher than highest_cost.
-
 ## If it is, update fanciest to this flavor and highest_cost to its price.
-
+for flavor in flavors:                     # loop through each flavor
+    if flavors[flavor] > highest_cost:     # compare price
+        print("The {} flavor costs ${:.2f}, which is greater than the highest_cost.".format(flavor, flavors[flavor]))
+## If it is, update fanciest to this flavor and highest_cost to its price.
 ## After the loop, print the most expensive flavor.
+    
+for flavor in flavors:
+    if flavors[flavor] > highest_cost:
+        highest_cost = flavors[flavor]   # update highest price
+        fanciest = flavor                # update flavor name
+        print(flavor, "is now the highest at $", highest_cost)
+
+print("\nThe fanciest flavor is:", fanciest)
+print("It costs $", highest_cost)
--- collections/exercises/strings.py
+++ collections/exercises/strings.py
@@ -4,22 +4,38 @@ text = 'Strings_are_sequences_of_characters.'
 word = 'tomato'
 
 # 1. Print a slice of the first 12 characters from 'text'.
-
+length_of_string = len(text)
+print(length_of_string)
+new_text = text[0:12]
+print(new_text)
 
 # 2. Print a slice of the last 12 characters from 'text'. You should NOT have to count the index values yourself!
 
+reverse_text = text[-12:]
+print(reverse_text)
 
 # 3. Print a slice of the middle 12 characters from 'text'.
 
+
+print(text[12:24])
 
 # ---- Exercise 3: Looping Through a String ----
 
 # Use index values to loop backwards through 'word'.
 
 # 1. Print 1 letter per line.
-
+print('Looping:')
+for words in word:
+    print('\t'+ words)
 
 # 2. Refactor the code to use the accumulator pattern to build up and print the reversed string. For example, if given 'good', print 'doog' on one line.
+print('Reverse Looping:')
+for words in word[::-1]:   
+    print(words,end='\n')
+
+# 3. Refactor the code to print a combination of the original and reversed string. For example, given 'tomato', print 'tomatootamot'.
+#  (If you want to be fancy, print 'tomato | otamot').
 
 
-# 3. Refactor the code to print a combination of the original and reversed string. For example, given 'tomato', print 'tomatootamot'. (If you want to be fancy, print 'tomato | otamot').
+print("Concate:")
+print(word+ "|" +word[::-1])
